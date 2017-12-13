69c6671bb332574c6292e3c366f753ce4b5f2184
lupc-14 <avinashi.luminoguru@gmail.com>
2017-12-13 14:32:02 +0530

    team controller made

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -5,3 +5,12 @@
  */
 Route::redirect('/', '/admin/dashboard', 301);
 Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::resource('team', 'TeamController');
+
+Route::get('work_flow', 'DashboardController@workFlow')->name('workFlow');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
