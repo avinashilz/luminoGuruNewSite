7a58d391f1ad718b62c7c1f81676123f7b779000
Rahul Behl <rahulbehl91@gmail.com>
2017-12-14 18:38:54 +0530

    corrected breadcrumbs

--- routes/breadcrumbs/backend/projects.php
+++ routes/breadcrumbs/backend/projects.php
@@ -1,20 +1,20 @@
 <?php
 
-Breadcrumbs::register('admin.team-members.index', function ($breadcrumbs) {
-    $breadcrumbs->push('Team Members', route('admin.team-members.index'));
+Breadcrumbs::register('admin.projects.index', function ($breadcrumbs) {
+    $breadcrumbs->push('Projects', route('admin.projects.index'));
 });
 
-Breadcrumbs::register('admin.team-members.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin.team-members.index');
-    $breadcrumbs->push('Add Team Member', route('admin.team-members.create'));
+Breadcrumbs::register('admin.projects.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('admin.projects.index');
+    $breadcrumbs->push('Add Project', route('admin.projects.create'));
 });
 
-Breadcrumbs::register('admin.team-members.edit', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin.team-members.index');
-    $breadcrumbs->push('Edit Team Member');
+Breadcrumbs::register('admin.projects.edit', function ($breadcrumbs) {
+    $breadcrumbs->parent('admin.projects.index');
+    $breadcrumbs->push('Edit Project');
 });
 
-Breadcrumbs::register('admin.team-members.show', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin.team-members.index');
-    $breadcrumbs->push('Show Team Member');
+Breadcrumbs::register('admin.projects.show', function ($breadcrumbs) {
+    $breadcrumbs->parent('admin.projects.index');
+    $breadcrumbs->push('Show Project');
 });
