b47cd984b13b83802d0f0e59588976f752fd8f29
Rahul Behl <rahulbehl91@gmail.com>
2017-12-14 16:05:12 +0530

    added contact edit form

---
+++ resources/views/backend/contacts/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('backend.layouts.app')
+
+@section ('title', __('labels.backend.access.users.management') . ' | ' . __('labels.backend.access.users.edit'))
+
+@section('breadcrumb-links')
+@include('backend.team-members.includes.breadcrumb-links')
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <strong>{{ isset($contact) ? 'Edit' : 'Add' }} Team Member</strong>
+            </div><!--card-header-->
+            <div class="card-block">
+                {{ html()->modelForm($contact, 'PUT', route('admin.contacts.update', $contact->id))->class('form-horizontal')->acceptsFiles()->open() }}
+                <div class="form-group row">
+                    <label class="col-md-3 form-control-label" for="name">Address</label>
+                    <div class="col-md-9">
+                        {{ html()->text('address')->placeholder('Address')->class('form-control')->required(true) }}
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 form-control-label" for="name">Phone No</label>
+                    <div class="col-md-9">
+                        {{ html()->text('phone_no')->placeholder('Phone No')->class('form-control')->required(true) }}
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 form-control-label" for="working_hour">Working Hour</label>
+                    <div class="col-md-9">
+                        {{ html()->text('working_hour')->placeholder('Working Hour')->class('form-control')->required(true) }}
+                    </div>
+                </div>
+                {{ html()->submit('Submit')->class('pull-right') }}
+                {{ html()->form()->close() }}
+            </div><!--card-block-->
+        </div><!--card-->
+    </div><!--col-->
+</div><!--row-->
+@endsection
