041d9ca4ed8dd1b0441801125aca4165074ef951
Rahul Behl <rahulbehl91@gmail.com>
2017-12-14 14:03:59 +0530

    updated sidebar

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -14,6 +14,132 @@
             </li>
 
             @if ($logged_in_user->isAdmin())
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> Team Members
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Team Members
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add Team Member
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> Projects
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Projects
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add Project
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> Partners
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Partners
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add Service
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> Testimonials
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Testimonials
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add Testimonials
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> Job Postings
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Job Postings
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add Job Posting
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> How we do it
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Steps
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add New Step
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
+                    <a class="nav-link nav-dropdown-toggle" href="#">
+                        <i class="icon-user"></i> Services
+                    </a>
+
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
+                                All Services
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
+                                Add Service
+                            </a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/auth*'), 'open') }}">
                     <a class="nav-link nav-dropdown-toggle" href="#">
                         <i class="icon-user"></i> {{ __('menus.backend.access.title') }}
@@ -36,24 +162,6 @@
                         <li class="nav-item">
                             <a class="nav-link {{ active_class(Active::checkUriPattern('admin/auth/role*')) }}" href="{{ route('admin.auth.role.index') }}">
                                 {{ __('labels.backend.access.roles.management') }}
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown {{ active_class(Active::checkUriPattern('admin/team-members*'), 'open') }}">
-                    <a class="nav-link nav-dropdown-toggle" href="#">
-                        <i class="icon-user"></i> Team Members
-                    </a>
-
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members')) }}" href="{{ route('admin.team-members.index') }}">
-                                All Team Members
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ active_class(Active::checkUriPattern('admin/team-members/create')) }}" href="{{ route('admin.team-members.create') }}">
-                                Add Team Member
                             </a>
                         </li>
                     </ul>
