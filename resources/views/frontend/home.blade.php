6702a2ecf3d49bce48f0301a0a273b28e9af90ef
luminoguru <luminoguru@luminoguru-Vostro-15-3568>
2018-04-13 16:33:26 +0530

    -create home.css page and add in webpack.mix.js -create home.blade.php page in layout. -

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,6 +1,25 @@
-<div class="row">
-   <ul>
-      <li>Contact@luminoguru.com</li>
-       <li>Contact@luminoguru.com</li>
-   </ul>
-</div>
+@extends('frontend.layouts.home')
+
+@section('content')
+<div class="container" id="header">  
+   <header>
+      <div class="row contact-luminogru">
+         <div class="col-sm-6 email-id">
+            <p>Contact@luminoguru.com</p>
+         </div>
+         <div class="col-sm-6 tel-no">
+            <p>+1234567889</p>  
+         </div>
+      </div>
+      <div class="row digital-craetive-agency">
+         <div class="col-sm-6">
+            <p>Contact@luminoguru.com</p>
+         </div>
+         <div class="col-sm-6 tel-no">
+            <p>+1234567889</p>  
+         </div>
+      </div>
+   </header>
+</div>  
+
+@endsection  
---
+++ resources/views/frontend/layouts/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+     <head>
+      <meta charset="utf-8">
+      <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Home</title>
+      <!-- Bootstrap -->  
+      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <!-- poppins -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+      <!-- font-awesome-css -->
+      <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+         <!-- home-css -->
+      <link href="css/frontend/home.css" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+
+      @yield('content')
+
+      <script src="js/jquery.js"></script>
+      <script src="bootstrap/js/bootstrap.min.js"></script>
+      <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+      <script src="js/script.js"></script>
+   </body>
+
+</html>
